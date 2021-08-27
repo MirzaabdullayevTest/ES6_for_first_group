6cff09ff58d9c700c5d280fc18439e8946b0a71c
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2021-08-27 11:36:29 +0500

    second lesson

--- index.js
+++ index.js
@@ -132,3 +132,149 @@ setTimeout
 //     { name: 'Con', age: 70, nickname: 'Bill Gates' },
 // ]
 // users.forEach(user => console.log(user.nickname))
+
+// ========================================== $ string
+// const person = {
+//     name: 'Jack',
+//     age: 20,
+//     number: 998975552233
+// }
+
+// const { name, age, number } = person
+
+// // console.log('Name is ' + name + ' ' + 'Age is ' + age + ' ' + 'Number is ' + number);
+// console.log(`Name is ${name}, age = ${age}, number = ${number}`);
+
+//========================================== spread operatori // rest
+// const numbers = [5, 6, 2, 3, 10]
+
+// const calc = (a, b, c, ...rest) => {
+//     return rest
+// }
+
+// // const sum = calc(numbers[0], numbers[1])
+// const sum = calc(...numbers)
+
+// // console.log(`Natija: ${sum}`);
+// console.log(sum);
+
+// const numbers = [5, 6, 2, 3, 10]
+
+// // numbers.forEach((val, index) => {
+// //     console.log(val);
+// // })
+
+// console.log(...numbers);
+
+// const person = {
+//     name: 'Tom',
+//     age: 65
+// }
+
+// const common = {
+//     number: 99898,
+//     job: 'Solar copmany'
+// }
+
+// const obj = {...person, ...common}
+// console.log(obj.age);
+
+// rest object ===========
+// const person = {
+//     name: 'Tom',
+//     age: 65
+// }
+
+// const common = {
+//     number: 99898,
+//     job: 'Solar copmany'
+// }
+
+// const obj = { ...person, ...common }
+
+// const { name, ...rest } = obj
+
+// console.log(rest);
+
+// Class ========== 
+
+// function Car(marka) {
+//     this.marka = marka
+
+//     console.log(this.marka);
+// }
+
+// const ford = new Car('Ford')
+
+// class Car {
+//     constructor(marka, year) {
+//         this.marka = marka
+//         this.year = year
+//     }
+//     logger() {
+//         console.log(this.marka, this.year);
+//     }
+// }
+// const car = new Car('BMW', 1990)
+// car.logger()
+
+// ================================== default parametrlar
+// const createPost = (str1, str2 = 'Default text', date = new Date().toDateString()) => {
+//     return {
+//         str1: str1,
+//         str2: str2,
+//         // date: date || new Date().toLocaleDateString()
+//         date: date
+//     }
+// }
+
+// const post = createPost('Hello world')
+// console.log(post);
+
+// map ------------------------------------
+
+// const arr = [1, 2, 3, 4, 5, 6]
+
+// arr.map((val, index) => {
+//     console.log(val);
+// })
+
+// arr.forEach((val, index) => {
+//     console.log(val);
+// })
+
+//  ================== filter
+// const arr = [1, 2, 3, 4, 5, 6]
+// const filter = arr.filter((val, index) => {
+//     if (val % 2 !== 1){
+//         return true
+//     }else{
+//         return false
+//     }
+// })
+
+// console.log(filter);
+
+// const users = [
+//     { name: 'Jack', age: 18, job: true },
+//     { name: 'Tom', age: 50, job: true },
+//     { name: 'Alex', age: 60, job: false },
+//     { name: 'Martin', age: 10, job: false },
+//     { name: 'Riko', age: 12, job: false },
+// ]
+
+// const adult = users.filter((val, index) => {
+//     if (!val.job) {
+//         return true
+//     } else {
+//         return false
+//     }
+// })
+
+// console.log(adult);
+
+/*
+
+ Client -> Server -> DateBase -> Server -> Client
+
+*/
